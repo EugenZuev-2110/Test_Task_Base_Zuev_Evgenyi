dbe947715167e319b87c4a2a31e45e47ff2245a3
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-21 04:40:49 +0300

    IBookService added

---
+++ Test_Task_Base_Zuev_Evgenyi/Services/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using Test_Task_Base_Zuev_Evgenyi.Models;
+
+namespace Test_Task_Base_Zuev_Evgenyi.Services;
+
+public interface IBookService
+{
+    Task<IEnumerable<Book>> GetAllOrSearchAsync(string? searchQuery);
+    Task<Book?> GetByIdAsync(int id);
+    Task CreateAsync(Book book);
+    Task UpdateAsync(Book book);
+    Task DeleteAsync(int id);
+}
