c29c167e0fe91ae29ac20f60bb298698214af8e9
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-21 05:13:26 +0300

    Middleware added

--- Test_Task_Base_Zuev_Evgenyi/Program.cs
+++ Test_Task_Base_Zuev_Evgenyi/Program.cs
@@ -1,6 +1,19 @@
+using Test_Task_Base_Zuev_Evgenyi.Data;
+using Test_Task_Base_Zuev_Evgenyi.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBookService, BookService>();
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
