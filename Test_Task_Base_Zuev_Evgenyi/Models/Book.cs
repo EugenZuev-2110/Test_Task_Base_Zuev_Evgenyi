a6899009d93415c45115d54f713ea7ea0650816c
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-21 04:30:35 +0300

    Model Book created

---
+++ Test_Task_Base_Zuev_Evgenyi/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test_Task_Base_Zuev_Evgenyi.Models;
+
+[Table("Books")]
+public class Book
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(250)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(250)]
+    public string Author { get; set; } = string.Empty;
+
+    [Required]
+    public int PublishYear { get; set; }
+
+    [Column(TypeName = "xml")]
+    public string? TableOfContents { get; set; }
+
+    [MaxLength(20)]
+    public string? ISBN { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
