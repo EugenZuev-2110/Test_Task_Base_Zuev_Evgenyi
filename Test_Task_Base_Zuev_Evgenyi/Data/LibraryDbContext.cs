353838524b841167f4c8addf58d65d96b7bab2b4
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-21 04:37:57 +0300

    LibraryDbContext added

---
+++ Test_Task_Base_Zuev_Evgenyi/Data/LibraryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Task_Base_Zuev_Evgenyi.Models;
+
+namespace Test_Task_Base_Zuev_Evgenyi.Data;
+
+public class LibraryDbContext : DbContext
+{
+    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books => Set<Book>();
+}
