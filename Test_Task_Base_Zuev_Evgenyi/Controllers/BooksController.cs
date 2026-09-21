464742165fc4a9e57f3cbbfc18eea2b94910b951
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-21 04:48:31 +0300

    BooksController added

---
+++ Test_Task_Base_Zuev_Evgenyi/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_Task_Base_Zuev_Evgenyi.Models;
+using Test_Task_Base_Zuev_Evgenyi.Services;
+
+namespace Test_Task_Base_Zuev_Evgenyi.Controllers;
+
+[ApiController]
+[Route("api/books")]
+public class BooksController : ControllerBase
+{
+    private readonly IBookService _bookService;
+    public BooksController(IBookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] string? query)
+    {
+        var result = await _bookService.GetAllOrSearchAsync(query);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Book book)
+    {
+        await _bookService.CreateAsync(book);
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] Book book)
+    {
+        book.Id = id;
+        await _bookService.UpdateAsync(book);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _bookService.DeleteAsync(id);
+        return Ok();
+    }
+}
