a40a7a27af2625b1cae48f5513f88b5506740b8b
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-21 05:23:55 +0300

    OnModelCreating added

--- Test_Task_Base_Zuev_Evgenyi/Data/LibraryDbContext.cs
+++ Test_Task_Base_Zuev_Evgenyi/Data/LibraryDbContext.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Test_Task_Base_Zuev_Evgenyi.Models;
 
-namespace Test_Task_Base_Zuev_Evgenyi.Data;
-
-public class LibraryDbContext : DbContext
+namespace Test_Task_Base_Zuev_Evgenyi.Data
 {
-    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+    public class LibraryDbContext : DbContext
     {
-    }
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Books => Set<Book>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-    public DbSet<Book> Books => Set<Book>();
+            modelBuilder.Entity<Book>()
+                .Property(b => b.TableOfContents)
+                .HasColumnType("xml");
+        }
+    }
 }
